4d264f379e0626e2a9e24c7770305f3d475c3d0f
Maxwell Enger <38050331+MaxEnger@users.noreply.github.com>
2019-09-18 12:19:47 -0400

    Create Run-Off.py

---
+++ Run-Off.py
@@ -0,0 +1,25 @@
+# Max Enger
+# Lab 2: Run-Off Conversion
+# Time to complete: 20 min
+# Description: Converting the run-off on a 50x20ft plot of land in Kenya
+
+#Converting feet to inches
+l= 50*12
+w= 20*12
+
+#Calculating Area of Plot in inches
+plot1_area = l*w
+
+#Calculating number of inches of rainfall in cubic inches of water
+r = (plot1_area*1)
+
+#Converting cubic inches to gallons
+gal_conv = 231
+r_gal = (r / gal_conv)
+
+#Final Outputs
+print 'The final outputs for lab 3 are below:'
+print 'plot_length is:',l
+print 'plot_width is:',w
+print 'rainfall_inches is:',r
+print 'runoff_gallons is:',r_gal
